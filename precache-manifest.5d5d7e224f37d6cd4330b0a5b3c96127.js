92ba01bf5f6246b62b6d975b1b5c1b9c96ea85de
Mahesh Nama <you@example.com>
2019-12-11 03:02:03 +0530

    Updates

---
+++ precache-manifest.5d5d7e224f37d6cd4330b0a5b3c96127.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a82c20278c4d6cc33443",
+    "url": "/Luezoid-task-/static/js/runtime~main.a82c2027.js"
+  },
+  {
+    "revision": "52bb1fdca7c4df07c55f",
+    "url": "/Luezoid-task-/static/js/main.52bb1fdc.chunk.js"
+  },
+  {
+    "revision": "ed1a98a028dd73953538",
+    "url": "/Luezoid-task-/static/js/1.ed1a98a0.chunk.js"
+  },
+  {
+    "revision": "52bb1fdca7c4df07c55f",
+    "url": "/Luezoid-task-/static/css/main.bfb2e6d9.chunk.css"
+  },
+  {
+    "revision": "7e4daf4d84cb3ca6a328751c19a96cf8",
+    "url": "/Luezoid-task-/index.html"
+  }
+];
